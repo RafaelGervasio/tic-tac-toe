0ebc5938c7179ff39612a375b7566454832f45a1
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-10-04 14:14:47 +0100

    Game is working now. I just need to make it playable from the command line

--- app.rb
+++ app.rb
@@ -1,115 +1,5 @@
-# Players need to be able to select (1) Circle or Cross (2) Where of the 9 squares to put it
-#     They can't put it in a place where there's already something else
-
-# There should be a class called game that initializes the board, and the two players
-#It should also contain the rules, to be checked each time a player makes a move
-
-#There should be a class called players that allows them to actually play
-    #A method called play, where they select X or O and from 1-9 Where it goes
-
-#p1 uses crosses
-#p2 uses circles
-
-
 #TODO
-#Display the 3 arrays as vertical, not horizontal -> Use /n probably
-
-#Create game rules, to evalute if someone won (each round evaluate it), and if so, display the winner
-
-#Make it so once the board is full game_rules() (that should me a method!) is automatically evaluated, and results are displed, 
-#and everything resets
-
-
-# class Game
-#     attr_accessor :name
-    
-#     @@board  = [
-#         [nil, nil, nil],
-#         [nil, nil, nil],
-#         [nil, nil, nil],
-#     ]
-    
-#     players = []
-    
-#     def initialize (name)
-#         @name = name
-#     end
-
-#     def evaluate_game
-#         #Evaluate
-#         countX = 0
-#         countO = 0
-#         winner = 0
-
-#         @@board.each do |arr|
-#             arr.each do |elem|
-#                 if elem == 'X'
-#                     countX+=1
-#                 else
-#                     countO +=1
-#                 end
-#             end
-#             if countX == 3
-#                 winner = players[0]
-#             elsif countO == 3
-#                 winner = players[1]
-#             end
-#         end
-
-#         unless winner==0
-#             puts "The winner is #{winner}!"
-#         end
-#     end
-
-
-# end
-
-# class Players < Game
-
-#     attr_reader :player_name
-
-#     @@players = []
-#     def self.all
-#         @@players
-#     end
-    
-    
-#     def initialize (player_name)
-#         @player_name = player_name
-#         @@players.push(@player_name) 
-#         players.push(@player_name)
-#     end
-
-
-#     def play_move(position1, position2)
-#         if self.player_name == @@players[0]
-#             @@board[position1][position2] = 'X'
-#         else
-#             @@board[position1][position2] = 'O'
-#         end
-#         p @@board
-#         evaluate_game()
-#     end
-# end
-
-# my_game = Game.new('my_game')
-# rafa = Players.new('Rafa')
-# jon = Players.new('Jon')
-
-# rafa.play_move(0, 0)
-# rafa.play_move(0, 1)
-# rafa.play_move(0, 2)
-
-
-
-
-#Let me re-think the design
-
-# - I need a board
-# - I need two players
-# - I need a way to evaluate each round, and display the winner if needed
-# - I need a way for players to play and have the board be updated
-
+#Make it into a command line game
 
 
 class TicTacToe
@@ -132,31 +22,69 @@ class TicTacToe
             [position1][position2] = 'O'
         end
 
-        p @board
+        @board.each {|row| p row}
+        puts '---------'
 
         evaluate_game()
     end
 
     def evaluate_game
+        
         countX = 0
         countO = 0
         winner = ''
 
+        #Horizontal check
         @board.each do |arr|
             arr.each do |elem|
                 if elem == 'X'
                     countX+=1
-                else
+                elsif elem =='O'
                     countO +=1
                 end
             end
+
             if countX == 3
                 winner = self.p1
             elsif countO == 3
                 winner = self.p2
+            else
+                countO = 0
+                countX = 0
             end
         end
 
+        #Vertical check
+        flat_board = @board.flatten
+        if(
+            (flat_board[0] == 'X' && flat_board[3] == 'X' && flat_board[6] == 'X') or
+            (flat_board[1] == 'X' && flat_board[4] == 'X' && flat_board[7] == 'X') or
+            (flat_board[2] == 'X' && flat_board[5] == 'X' && flat_board[8] == 'X')
+        )
+            winner = self.p1
+        
+        elsif(
+            (flat_board[0] == 'O' && flat_board[3] == 'O' && flat_board[6] == 'O') or
+            (flat_board[1] == 'O' && flat_board[4] == 'O' && flat_board[7] == 'O') or
+            (flat_board[2] == 'O' && flat_board[5] == 'O' && flat_board[8] == 'O')
+        )
+            winner = self.p2
+        end
+
+        #Diagonal check
+        if(
+            (flat_board[0] == 'X' && flat_board[4] == 'X' && flat_board[8] == 'X') or
+            (flat_board[2] == 'X' && flat_board[4] == 'X' && flat_board[6] == 'X') 
+        )
+            winner = self.p1
+        
+        elsif(
+            (flat_board[0] == 'O' && flat_board[4] == 'O' && flat_board[8] == 'O') or
+            (flat_board[2] == 'O' && flat_board[4] == 'O' && flat_board[6] == '') 
+        )
+            winner = self.p2
+        end
+        
         unless winner==''
             puts "The winner is #{winner}!"
         end
@@ -167,9 +95,3 @@ end
 
 
 t = TicTacToe.new('Rafa', 'Jon')
-p t.p1
-p t.p2
-
-t.play_round('Rafa', 0, 0)
-t.play_round('Rafa', 0, 1)
-t.play_round('Rafa', 0, 2)
