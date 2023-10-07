7d85e9cf621442897ff04806f5f4fbad34909eb5
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-10-07 19:22:08 +0100

    Game finished. Some small issues, but the OOP stuff is 10x better

--- app.rb
+++ app.rb
@@ -1,107 +1,94 @@
 class TicTacToe
-    attr_reader :p1, :p2
-    
-    def initialize (p1, p2)
-        @p1 = p1
-        @p2 = p2
-        @board = [
-            [nil, nil, nil],
-            [nil, nil, nil],
-            [nil, nil, nil],
-        ]
-    end
+  attr_reader :p1, :p2
 
-    def play_round(player, position1, position2)
-        if self.p1 == player
-            @board[position1][position2] = 'X'
-        else
-            @board[position1][position2] = 'O'
+  def initialize(p1, p2)
+    @p1 = p1
+    @p2 = p2
+    @board = [
+      [nil, nil, nil],
+      [nil, nil, nil],
+      [nil, nil, nil]
+    ]
+  end
+
+  def play_round(player, position1, position2)
+    @board[position1][position2] = if p1 == player
+                                     'X'
+                                   else
+                                     'O'
+                                   end
+
+    @board.each { |row| p row }
+    puts '---------'
+
+    evaluate_game
+  end
+
+  def evaluate_game
+    countX = 0
+    countO = 0
+    winner = ''
+
+    # Horizontal check
+    @board.each do |arr|
+      arr.each do |elem|
+        if elem == 'X'
+          countX += 1
+        elsif elem == 'O'
+          countO += 1
         end
+      end
 
-        @board.each {|row| p row}
-        puts '---------'
-
-        evaluate_game()
-    end
-
-    def evaluate_game
-        
-        countX = 0
+      if countX == 3
+        winner = p1
+      elsif countO == 3
+        winner = p2
+      else
         countO = 0
-        winner = ''
-
-        #Horizontal check
-        @board.each do |arr|
-            arr.each do |elem|
-                if elem == 'X'
-                    countX+=1
-                elsif elem =='O'
-                    countO +=1
-                end
-            end
-
-            if countX == 3
-                winner = self.p1
-            elsif countO == 3
-                winner = self.p2
-            else
-                countO = 0
-                countX = 0
-            end
-        end
-
-        #Vertical check
-        flat_board = @board.flatten
-        if(
-            (flat_board[0] == 'X' && flat_board[3] == 'X' && flat_board[6] == 'X') or
-            (flat_board[1] == 'X' && flat_board[4] == 'X' && flat_board[7] == 'X') or
-            (flat_board[2] == 'X' && flat_board[5] == 'X' && flat_board[8] == 'X')
-        )
-            winner = self.p1
-        
-        elsif(
-            (flat_board[0] == 'O' && flat_board[3] == 'O' && flat_board[6] == 'O') or
-            (flat_board[1] == 'O' && flat_board[4] == 'O' && flat_board[7] == 'O') or
-            (flat_board[2] == 'O' && flat_board[5] == 'O' && flat_board[8] == 'O')
-        )
-            winner = self.p2
-        end
-
-        #Diagonal check
-        if(
-            (flat_board[0] == 'X' && flat_board[4] == 'X' && flat_board[8] == 'X') or
-            (flat_board[2] == 'X' && flat_board[4] == 'X' && flat_board[6] == 'X') 
-        )
-            winner = self.p1
-        
-        elsif(
-            (flat_board[0] == 'O' && flat_board[4] == 'O' && flat_board[8] == 'O') or
-            (flat_board[2] == 'O' && flat_board[4] == 'O' && flat_board[6] == '') 
-        )
-            winner = self.p2
-        end
-        
-        unless winner==''
-            puts "The winner is #{winner}!"
-        end
-        
-        total_rounds = 0
-        flat_board.each do |elem|
-            if elem=='O' or elem=='X'
-                total_rounds+=1
-            end
-        end
-
-        if total_rounds == 9
-            winner = 0
-            puts "The game is a draw!"
-        end
-
+        countX = 0
+      end
     end
 
+    # Vertical check
+    flat_board = @board.flatten
+    if (flat_board[0] == 'X' && flat_board[3] == 'X' && flat_board[6] == 'X') or
+       (flat_board[1] == 'X' && flat_board[4] == 'X' && flat_board[7] == 'X') or
+       (flat_board[2] == 'X' && flat_board[5] == 'X' && flat_board[8] == 'X')
 
+      winner = p1
+
+    elsif (flat_board[0] == 'O' && flat_board[3] == 'O' && flat_board[6] == 'O') or
+          (flat_board[1] == 'O' && flat_board[4] == 'O' && flat_board[7] == 'O') or
+          (flat_board[2] == 'O' && flat_board[5] == 'O' && flat_board[8] == 'O')
+
+      winner = p2
+    end
+
+    # Diagonal check
+    if (flat_board[0] == 'X' && flat_board[4] == 'X' && flat_board[8] == 'X') or
+       (flat_board[2] == 'X' && flat_board[4] == 'X' && flat_board[6] == 'X')
+
+      winner = p1
+
+    elsif (flat_board[0] == 'O' && flat_board[4] == 'O' && flat_board[8] == 'O') or
+          (flat_board[2] == 'O' && flat_board[4] == 'O' && flat_board[6] == '')
+
+      winner = p2
+    end
+
+    puts "The winner is #{winner}!" unless winner == ''
+
+    total_rounds = 0
+    flat_board.each do |elem|
+      total_rounds += 1 if %w[O X].include?(elem)
+    end
+
+    return unless total_rounds == 9
+
+    winner = 0
+    puts 'The game is a draw!'
+  end
 end
-
 
 t = TicTacToe.new('Rafa', 'Jon')
 
--- refactor.rb
+++ refactor.rb
@@ -1,121 +1,141 @@
 module Board
+    attr_reader :board
+    
     def create_board
-        attr_reader :board
-
         @board = [
-            ['-- Game Board --'],
-            [nil, nil, nil],
-            [nil, nil, nil],
-            [nil, nil, nil]
+            ['------Game Board------'],
+            ['empty', 'empty', 'empty'],
+            ['empty', 'empty', 'empty'],
+            ['empty', 'empty', 'empty']
         ]
+        puts "Welcome to Tic Tac Toe!!"
+        puts "------------------------"
+        puts "------------------------"
     end
     
     def display_board
-        board.each {|row| puts row}
-        puts ''
+        board.each { |row| p row }
+        puts '---------'
     end
     
     def game_over?
         flat_board = board.flatten[1..-1]
-        
-        i=0
-        3.times do
-            if (flat_board[i] == flat_board[i+1] == flat_board [i+2] && flat_board[i] != nil)
-                return true
-            i+=3
+      
+        # Check rows
+        (0..2).each do |i|
+          if flat_board[i] == flat_board[i + 1] && flat_board[i] == flat_board[i + 2] && flat_board[i] != 'empty'
+            if flat_board[i] == 'X'
+                puts "The winner is #{@p1}!!!"
+            else
+                puts "The winner is #{@p2}!!!"
+            return true
+            end
+          end
+        end
+      
+        # Check columns
+        (0..2).each do |i|
+          if flat_board[i] == flat_board[i + 3] && flat_board[i] == flat_board[i + 6] && flat_board[i] != 'empty'
+            if flat_board[i] == 'X'
+                puts "The winner is #{@p1}!!!"
+            else
+                puts "The winner is #{@p2}!!!"
+            return true
+            end
+          end
+        end
+      
+        # Check diagonals
+        if (flat_board[0] == flat_board[4] && flat_board[0] == flat_board[8] && flat_board[0] != 'empty') ||
+           (flat_board[2] == flat_board[4] && flat_board[2] == flat_board[6] && flat_board[2] != 'empty')
+           if flat_board[i] == 'X'
+            puts "The winner is #{@p1}!!!"
+            else
+            puts "The winner is #{@p2}!!!"
+            return true
             end
         end
-        
-        i=0
-        3.times do
-            if (flat_board[i] == flat_board[i+3] == flat_board [i+6] && flat_board[i] != nil)
-                return true
-            i+=1
-            end
-        end
-
-        if (flat_board[0] == flat_board[4] == flat_board[8] && flat_board[0] != nil)
-            return true
-        elsif (flat_board[2] == flat_board[4] == flat_board[6] && flat_board[2] != nil)
-            return true
-        end
-        
-        return false
-
+      
+        false
+      end
     end
 
-
 module Players
-    attr_reader :p1
-    attr_reader :p2
-
     @p1 = ''
     @p2 = ''
 
     def create_player
         puts "What's your name?"
         reply = gets.chomp
-        if !!p1 == false
-            p1 = reply
+        if @p1 == nil
+            @p1 = reply
         else
-            p2 = reply
+            @p2 = reply
         end
     end
 end
 
 
 class Game
-    @turn = 1
-    attr_reader :turn
-
     include Board
     include Players
 
     def initialize
-        
+        @turn = 1
+        start_game()
     end
 
-    def current_turn (turn=1)
-        if turn==1
-            return self.p1
-        elsif turn==-1
-            return self.p2
+    def current_turn
+        if @turn==1
+            return @p1
+        elsif @turn==-1
+            return @p2
         end
     end
     
     def make_move(row, collumn)
-        if current_turn() == self.p1
+        if current_turn() == @p1
             board[row][collumn-1] = 'X'
         else
             board[row][collumn-1] = 'O'
         end
-        turn = -(turn)
+        @turn = -@turn
     end
 
     def start_game
         create_board()
         create_player()
         create_player()
+        current_turn()
 
-        unless game_over?()
+        i = 0
+        while game_over?() == false && i<9
             display_board()
-            if turn == 1
-                puts "#{p1}'s turn"
+            if @turn == 1
+                puts "It's #{@p1}'s turn:"
                 puts "What row do you want to put your 'X' in?"
                 p_row = gets.chomp
                 puts "What collumn do you want to put your 'X' in?"
                 p_collumn = gets.chomp
-                make_move(p_row, p_collumn)
-            elsif turn == -1
-                puts "#{p2}'s turn"
-                puts "What row do you want to put your 'X' in?"
+                make_move(p_row.to_i, p_collumn.to_i)
+            elsif @turn == -1
+                puts "It's #{@p2}'s turn"
+                puts "What row do you want to put your 'O' in?"
                 p_row = gets.chomp
-                puts "What collumn do you want to put your 'X' in?"
+                puts "What collumn do you want to put your 'O' in?"
                 p_collumn = gets.chomp
-                make_move(p_row, p_collumn)
+                make_move(p_row.to_i, p_collumn.to_i)
             end
-            display_board()
+            i+=1
+        end
+        a = game_over?
+        if !a
+            puts "The game is a draw!"
+            return
         end
     end
 
 end
+
+
+my_game = Game.new
